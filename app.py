6ad8b4c8dec3d3c9896a338e5b486f8969c8874c
matthew casey <matthew.casey@hotmail.co.uk>
2020-05-11 19:00:06 +0100

    Added: -Search bar using mongo full text $text -full view for recipes -editing recipes for authors -deleting recipes -like and unlike system

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_uploads import configure_uploads, IMAGES, UploadSet
+import json
 
 # App config
 app = Flask(__name__)
@@ -15,16 +16,22 @@ app.config['MONGO_URI'] = (
     "")
 app.config['SECRET_KEY'] = ''
 mongo = PyMongo(app)
-
-photos = UploadSet('photos', IMAGES)
+# Config for flask uploads
+photos = UploadSet('photos', (IMAGES))
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
 configure_uploads(app, photos)
 
-# Default home page
-@app.route('/')
+# Default home page with search fucntion
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    recipes = mongo.db.recipe.find()
-    return render_template('home.html', recipes=recipes)
+    if request.method == 'GET' or request.form.get('search') == '':
+        recipes = list(mongo.db.recipe.find())
+        return render_template('home.html', recipes=recipes)
+    else:
+        search = request.form.get('search')
+        recipes = mongo.db.recipe.find({'$text': {'$search': search}})
+        return render_template('home.html', recipes=recipes)
+
 
 # Allows logged in users to add recipes to the database
 @app.route('/add_recipe', methods=['GET', 'POST'])
@@ -36,15 +43,66 @@ def add_recipe():
             flash('You must be logged in to add a recipe! Please log in now.')
             return redirect(url_for('login'))
     elif request.method == 'POST':
-        if 'photo' not in request.files:
-            recipe_img = 'default.jpeg'
-        else:
-            req = request.form
-            recipe_img = photos.save(request.files['photo'])
+        recipe_img = photos.save(request.files['photo'])
+        req = request.form
         new_recipe = {'author': req.get('author'), 'recipe_name': req.get('recipe_name'), 'recipe_desc': req.get('recipe_desc'), 'recipe_method': req.get('recipe_method'),
-                      'recipe_ingredients': req.get('recipe_ingredients'), 'recipe_img': recipe_img, 'time': req.get('time'), 'serves': req.get('serves'), 'is_veggie': req.get('is_veggie'), 'is_vegan': req.get('is_vegan')}
+                      'recipe_ingredients': req.get('recipe_ingredients'), 'recipe_img': recipe_img, 'time': req.get('time'), 'serves': req.get('serves'), 'is_veggie': req.get('is_veggie'), 'is_vegan': req.get('is_vegan'), 'likes': 0}
         mongo.db.recipe.insert_one(new_recipe)
         return redirect(url_for('home'))
+
+
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+    recipe = mongo.db.recipe.find_one({'_id': ObjectId(recipe_id)})
+    recipe_img = f"/static/uploads/{recipe.get('recipe_img')}"
+    return render_template('viewrecipe.html', recipe=recipe, recipe_img=recipe_img)
+
+# Edit finctionality if recipe is your own
+@app.route('/edit_recipe/<recipe_id>', methods=['GET', 'POST'])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipe.find_one({'_id': ObjectId(recipe_id)})
+    if request.method == 'GET':
+        return render_template('editrecipe.html', recipe=recipe)
+    else:
+        req = request.form
+        update = {'$set': {'author': req.get('author'), 'recipe_name': req.get('recipe_name'), 'recipe_desc': req.get('recipe_desc'), 'recipe_method': req.get('recipe_method'),
+                           'recipe_ingredients': req.get('recipe_ingredients'), 'time': req.get('time'), 'serves': req.get('serves'), 'is_veggie': req.get('is_veggie'), 'is_vegan': req.get('is_vegan')}}
+        mongo.db.recipe.update_one(
+            {'_id': ObjectId(recipe_id)}, update)
+        return redirect(url_for('view_recipe', recipe_id=recipe_id))
+
+        # View recipe
+
+# Delete recipe if your own
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    recipe = mongo.db.recipe.find_one({'_id': ObjectId(recipe_id)})
+# Removes image from MongoDatabase
+    os.remove(os.path.join(
+        app.config['UPLOADED_PHOTOS_DEST'], recipe.get('recipe_img')))
+    mongo.db.recipe.delete_one({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('home'))
+
+# Like functionality
+@app.route('/like/<recipe_id>')
+def like(recipe_id):
+    if session.get('USERNAME'):
+        if mongo.db.users.find_one({'username': session.get('USERNAME'), 'likes': recipe_id}, {'_id': 1}) == None:
+            mongo.db.recipe.update_one({'_id': ObjectId(recipe_id)}, {
+                '$inc': {'likes': 1}})
+            mongo.db.users.update_one({'username': session['USERNAME']}, {
+                '$push': {'likes': recipe_id}})
+            flash('like added!')
+        else:
+            mongo.db.recipe.update_one({'_id': ObjectId(recipe_id)}, {
+                '$inc': {'likes': -1}})
+            mongo.db.users.update_one({'username': session['USERNAME']}, {
+                '$pull': {'likes': recipe_id}})
+            flash('like removed!')
+    else:
+        flash('You must be logged in to do that!')
+    return redirect(url_for('view_recipe', recipe_id=recipe_id))
+
 
 # Log in functionality
 @app.route('/login', methods=['GET', 'POST'])
@@ -103,6 +161,9 @@ def create_account():
             flash(
                 f'Your account: ({username}) was created and you have been logged in successfully!')
             return redirect(url_for('home'))
+        else:
+            flash('Your passwords must match! Please try again.')
+            return redirect(url_for('create_account'))
     elif request.method == 'GET' and session.get('USERNAME'):
         username = session.get('USERNAME')
         flash('You are already logged in, {username}.')
